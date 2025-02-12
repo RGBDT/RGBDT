fcfbd1304ae27a25acc35b59edb7a21149c08a17
RGBDT <clasp-duct-harmony@duck.com>
2025-02-12 13:28:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// Получаем элементы
-const menuToggle = document.getElementById('menuToggle');
+// Получаем элементы меню и значка
+const menuIcon = document.getElementById('menu-icon');
 const sidebar = document.getElementById('sidebar');
 
-// Добавляем обработчик события для кнопки
-menuToggle.addEventListener('click', function() {
-  sidebar.classList.toggle('active'); // Переключаем класс 'active'
-});
-
-// Закрываем меню при клике вне его области
-document.addEventListener('click', function(event) {
-  if (!sidebar.contains(event.target) && !menuToggle.contains(event.target)) {
-    sidebar.classList.remove('active');
-  }
+// Добавляем обработчик события клика на значок меню
+menuIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
 });
