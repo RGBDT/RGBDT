c524c4e1624c79adc033a00a8d8912c33a2b56e3
RGBDT <clasp-duct-harmony@duck.com>
2025-02-12 13:47:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// Получаем элементы меню и значка
-const menuIcon = document.getElementById('menu-icon');
-const sidebar = document.getElementById('sidebar');
+// Функция для загрузки содержимого файла в элемент
+function loadHTMLFile(elementId, filePath) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка загрузки файла: ' + response.status);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
 
-// Добавляем обработчик события клика на значок меню
-menuIcon.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
+// Загружаем хедер и футер на всех страницах
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTMLFile('header', 'header.html');
+    loadHTMLFile('footer', 'footer.html');
+
+    // Добавляем обработчик события клика на значок меню
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+
+    if (menuIcon && navLinks) {
+        menuIcon.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
 });
